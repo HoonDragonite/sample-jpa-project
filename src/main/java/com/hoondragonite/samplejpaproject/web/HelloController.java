b5f781971d1ff2f21777c60664c21d560c5e3115
HoonDragonite <lsoonnoon@gmail.com>
2021-05-26 23:53:54 +0900

    Feat: Hello Get Method Test

---
+++ src/main/java/com/hoondragonite/samplejpaproject/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.hoondragonite.samplejpaproject.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}

af4cbb94689b739e3e2145f136f5335383ba9109
HoonDragonite <lsoonnoon@gmail.com>
2021-05-26 22:30:01 +0900

    Initial commit

---
+++ src/main/java/com/hoondragonite/samplejpaproject/SampleJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hoondragonite.samplejpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJpaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleJpaProjectApplication.class, args);
+    }
+
+}
